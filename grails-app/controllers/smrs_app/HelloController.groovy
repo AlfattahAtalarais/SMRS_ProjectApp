18e0e9bd5e32823de384fdf18de7da670ff418a5
AlfattahAtalarais <alfattah@fairtech.com.sg>
2025-08-10 09:48:14 +0700

    third commit

---
+++ grails-app/controllers/smrs_app/HelloController.groovy
@@ -0,0 +1,12 @@
+package smrs_app
+
+class HelloController {
+
+    def index() {
+        [msg: "Ini halaman Index"]
+    }
+
+    def about() {
+        [msg: "Ini halaman About"]
+    }
+}
---
+++ src/test/groovy/smrs_app/HelloControllerSpec.groovy
@@ -0,0 +1,18 @@
+package smrs_app
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class HelloControllerSpec extends Specification implements ControllerUnitTest<HelloController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
